4c9e954bad6ab36a9bebcd97d135de6a32903e9a
Hugh Ingram <hughi94@Gmail.com>
2018-11-04 18:24:55 +0000

    create api client

--- app/src/main/java/uk/co/hughingram/ethscan/MainActivity.kt
+++ app/src/main/java/uk/co/hughingram/ethscan/MainActivity.kt
@@ -1,14 +1,10 @@
 package uk.co.hughingram.ethscan
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.util.Log
-import io.reactivex.Single
 import io.reactivex.rxkotlin.subscribeBy
-import io.reactivex.schedulers.Schedulers
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import uk.co.hughingram.ethscan.network.EtherScanService
+import uk.co.hughingram.ethscan.network.ApiClient
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,21 +15,14 @@ class MainActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-        Single.fromCallable {
-            val retrofit = Retrofit.Builder()
-                .baseUrl("https://api-rinkeby.etherscan.io/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
-            val service = retrofit.create(EtherScanService::class.java)
-            val transactionListCall = service.listTransactions("0xfFfa5813ED9a5DB4880D7303DB7d0cBe41bC771F")
-            transactionListCall.execute().body().toString()
-        }.subscribeOn(Schedulers.io())
-            .subscribeBy(
-                onSuccess = {
-                    Log.i("request-result", it)
+        val apiClient = ApiClient()
+        apiClient.getTransactionList("0xfFfa5813ED9a5DB4880D7303DB7d0cBe41bC771F")
+            .subscribeBy (
+                onSuccess= {
+                    Log.i("api-result", it.toString())
                 },
                 onError = {
-                    Log.e("request-result", "", it)
+
                 }
             )
     }
