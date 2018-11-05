80b42a3f92a5a6697c913004f59cdae64686a1c7
Hugh Ingram <hughi94@Gmail.com>
2018-11-05 00:07:31 +0000

    cache transaction list

--- app/src/main/java/uk/co/hughingram/ethscan/view/MainActivity.kt
+++ app/src/main/java/uk/co/hughingram/ethscan/view/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import androidx.navigation.findNavController
 import uk.co.hughingram.ethscan.R
+import uk.co.hughingram.ethscan.network.EthereumTransaction
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,5 +14,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onSupportNavigateUp() = findNavController(R.id.my_nav_host_fragment).navigateUp()
+
+    var cache: List<EthereumTransaction> = listOf()
 
 }
